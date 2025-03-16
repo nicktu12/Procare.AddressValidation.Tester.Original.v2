681cdc12aa24e5ca6effa198cab9658645df6289
Nick Teets <nickteets@gmail.com>
2025-03-16 14:04:36 -0600

    Initial commit.

---
+++ AddressValidationService.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AddressValidationService.cs" company="Procare Software, LLC">
+//     Copyright © 2021-2025 Procare Software, LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Procare.AddressValidation.Tester;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+[SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "This is instantiated via DI.")]
+internal sealed class AddressValidationService
+{
+    internal const string HttpClientName = "AddressValidationHttpClient";
+
+    private readonly IHttpClientFactory httpClientFactory;
+
+    public AddressValidationService(IHttpClientFactory httpClientFactory)
+    {
+        ArgumentNullException.ThrowIfNull(httpClientFactory, nameof(httpClientFactory));
+
+        this.httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<string> GetAddressesAsync(AddressValidationRequest request, CancellationToken token = default)
+    {
+        using HttpClient httpClient = this.httpClientFactory.CreateClient(HttpClientName);
+        using HttpRequestMessage httpRequest = request.ToHttpRequest();
+        using HttpResponseMessage response = await httpClient.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead, token).ConfigureAwait(false);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsStringAsync(token).ConfigureAwait(false);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Procare Software, LLC">
+//     Copyright © 2021-2025 Procare Software, LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Procare.AddressValidation.Tester;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+internal sealed class Program
+{
+    private static async Task Main(string[] args)
+    {
+        IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddLogging((logger) =>
+                {
+                    logger.AddSimpleConsole(options =>
+                    {
+                        options.SingleLine = true;
+                        options.TimestampFormat = "HH:mm:ss.fff ";
+                    });
+                });
+
+                services.AddHttpClient(AddressValidationService.HttpClientName, client =>
+                {
+                    client.BaseAddress = new("https://addresses.dev-procarepay.com");
+                });
+
+                services.AddTransient<AddressValidationService>();
+                services.AddHostedService((services) => new ConsoleRunner(
+                    services.GetRequiredKeyedService<ILogger<ConsoleRunner>>(null),
+                    services.GetRequiredService<IHostApplicationLifetime>(),
+                    services.GetRequiredKeyedService<AddressValidationService>(null),
+                    args));
+            });
+
+        await hostBuilder.RunConsoleAsync().ConfigureAwait(false);
+    }
+}
